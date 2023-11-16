46b03eb7a90a9efd10db1328c6be2d5133f4f8b2
seohan1010 <seohan1010@hotmail.com>
2023-11-16 12:46:48 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        width: "500px",
+        height: "500px",
+        border: "1px solid black",
+        margin: "auto",
+        textAlign: "center",
+        lineHeight: "500px",
+      }}
+    >
+      welcome to board project
     </div>
   );
 }
